196324fb51617a7d8336efca51583a384c69ed30
Tom Bergamaschi <tbergama@gmail.com>
2020-03-30 22:04:37 -0700

    fix conflict

--- transform/__init__.py
+++ transform/__init__.py
@@ -1,9 +1,4 @@
 import transform.craigslist
-<<<<<<< HEAD
 import transform.offerup
 import transform.ebay
-=======
-import transform.ebay
-import transform.offerup
->>>>>>> cdf6f19c363d2cb88e73d7162d259ac24d228b7b
 import transform.filter
