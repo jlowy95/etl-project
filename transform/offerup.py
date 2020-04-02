b2df46ce50209c75578c44158280288b23a99025
Eyes0fTheWorld <Kscheller@gmail.com>
2020-04-01 18:51:18 -0700

    fixed transform on price

--- transform/offerup.py
+++ transform/offerup.py
@@ -2,7 +2,7 @@ def clean(offerup_data):
 
     for item in offerup_data:
         item['title'] = item['title'].lower()
-        item['price'] = float(item['price'].replace('$','').replace(',','')) if item['location'] != None else float('0')
+        item['price'] = float(item['price'].replace('$','').replace(',','')) if '$' in item['location'] else float('0')
         item['location'] = item['location'].lower() + ', USA' if item['location'] != None else ''
 
     return offerup_data
