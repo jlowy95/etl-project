51dc5c3b613831c5d25d1356a90839ec982001e4
Eyes0fTheWorld <Kscheller@gmail.com>
2020-04-01 20:14:54 -0700

    commented out print

--- transform/offerup.py
+++ transform/offerup.py
@@ -1,7 +1,7 @@
 def clean(offerup_data):
 
     for item in offerup_data:
-        print(item['url'])
+        #print(item['url'])
         item['title'] = item['title'].lower()
         item['price'] = float(item['price'].replace('$','').replace(',','')) if '$' in item['price'] else float('0')
         item['location'] = item['location'].lower() + ', USA' if item['location'] != None else ''
