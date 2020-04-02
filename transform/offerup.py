76e036fc12d68faba5f749de503ad2be726fcb2c
Eyes0fTheWorld <Kscheller@gmail.com>
2020-04-01 18:37:56 -0700

    added try/except

--- transform/offerup.py
+++ transform/offerup.py
@@ -2,7 +2,7 @@ def clean(offerup_data):
 
     for item in offerup_data:
         item['title'] = item['title'].lower()
-        item['price'] = float(item['price'].replace('$','').replace(',',''))
+        item['price'] = float(item['price'].replace('$','').replace(',','')) if item['location'] != None else float('0')
         item['location'] = item['location'].lower() + ', USA' if item['location'] != None else ''
 
     return offerup_data
