3b2241b3f0af343af570f43f58fa62d3b03cc5ef
Tom Bergamaschi <tbergama@gmail.com>
2020-04-01 19:55:31 -0700

    add craigslist extract logic

--- extract/craigslist.py
+++ extract/craigslist.py
@@ -1,2 +1,60 @@
+from splinter import Browser
+from splinter.exceptions import ElementDoesNotExist
+from bs4 import BeautifulSoup
+import datetime as dt
+
+from config import path_to_chromedriver
+
+
 def scrape(search_term, result_limit):
-    return ['foobar']
+    executable_path = {'executable_path': path_to_chromedriver}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    location = "sacramento"
+    search_term = "skis"
+    bundle_duplicates = 1
+
+    url = f"http://www.craigslist.org/search/sss?query={search_term}&sort=rel"
+    browser.visit(url)
+
+    item_list = []
+
+    while True:
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+        results = soup.find_all('li', class_="result-row")
+        
+        for result in results:
+        
+            # Grab the link, title, and price
+            link = result.a['href']
+            title = result.find('a', class_="result-title").text
+            price = result.find('span', class_="result-price").text
+            datetime = result.find('time', class_="result-date")['datetime']
+
+            # Create a dictionary for the item
+            item_dict = {
+                "url": link,
+                "title": title,
+                "price": price,
+                "datetime": datetime,
+                "scrape_date": dt.datetime.now().strftime('%Y:%m:%d %H:%M:%S'),
+                "location": link.split('.')[0].split('//')[1]
+            }
+
+            item_list.append(item_dict)
+
+            if len(item_list) >= result_limit:
+                break
+
+        if len(item_list) >= result_limit:
+            break
+        
+        # Try the next page
+        try:
+            browser.click_link_by_partial_text('next')
+        except:
+            break
+
+    return item_list
+
