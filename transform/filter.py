1f03bedae957ab344ebceefa2aa6ca0a340a0405
jlowy95 <jlowy95@gmail.com>
2020-04-01 19:57:07 -0700

    Initial filter work

--- transform/filter.py
+++ transform/filter.py
@@ -1,2 +1,29 @@
 def drop_listings(cleaned_data):
 	return ['foobar']
+
+	'''
+	# Determination function:
+	# Returns True if listing does not meet requested criteria, otherwise returns False
+	
+	def determine(listing):
+
+		# Test price
+		try:
+			float(listing['price'])
+		except:
+			return True
+		
+		# Test location
+		location = listing['location'].split()
+		if location[0] == 'usa':
+			return True
+		elif len(location[1]) != 2:
+			return True
+		else:
+			return False
+
+
+	filtered_data = [x for x in cleaned_data if not determine(x)]
+
+	return filtered_data
+	'''
