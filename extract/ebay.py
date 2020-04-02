a9ecf36d86a22d9916aed9920def01bc0e062c51
jlowy95 <jlowy95@gmail.com>
2020-04-02 15:19:28 -0700

    Fixed ebay extract, removed console prints from t-offerup

--- extract/ebay.py
+++ extract/ebay.py
@@ -44,22 +44,36 @@ def scrape(search_term, result_limit):
     # Create list of listing URLs
     url_list = []
 
-    while counter <= result_limit and counter <= (int(page_count) * 50):
+    while counter <= (result_limit + 5) and counter <= (int(page_count) * 50):
+        # Reset Soup (for new pages)
+        time.sleep(0.1)
+        html = browser.html
+        soup = bs(html,'lxml')
+        
+
+        # Grab ul of listings
         ul = soup.find('ul',{'class':'srp-results'})
+        time.sleep(0.05)
+
+        # Create list of listings
         list_items = ul.find_all('li',{'class':'s-item'})
+
+        # Filter listings and add urls
         for item in list_items:
-            if counter <= result_limit:
+            if counter <= (result_limit + 5):
                 if item.find('div',{'class':'s-item__title--tagblock'}):
                     continue
                 else:
                     url_list.append(item.find('a',{'class':'s-item__link'})['href'])
                     counter += 1
+                    # print(f"Added item {counter}")
             else:
                 break
         try:
             browser.find_by_css('a.pagination__next').click()
         except ElementDoesNotExist:
             browser.find_by_css('a.x-pagination__control')[1].click()
+        # print("Next page")
 
     # Visit all URLs and scrape data to 'listings'
     listings = []
@@ -67,28 +81,36 @@ def scrape(search_term, result_limit):
     scrape_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     for url in url_list:
         browser.visit(url)
+        time.sleep(0.02)
         html = browser.html
         soup = bs(html,'lxml')
         
         url = url
-        title = soup.find('h1',{'id':'itemTitle'}).text.lower()
-        price = soup.find('span',{'itemprop':'price'}).text
-        location = soup.find('span',{'itemprop':'availableAtOrFrom'}).text
-        listing_dict = {
-            'url': url,
-            'title': title,
-            'price': price,
-            'location': location,
-            'source': source,
-            'scrape_date':scrape_date,
-            'search_term':search_term
-        }
-        listings.append(listing_dict)
+
+        # Check for expired listings, if not expired then proceed
+        if soup.find('section',{'class':'page-notice'}) == None and soup.find('span',{'class':'statusContent'}) == None:
+            try:
+                title = soup.find('h1',{'id':'itemTitle'}).text.lower()
+                price = soup.find('span',{'itemprop':'price'}).text
+                location = soup.find('span',{'itemprop':'availableAtOrFrom'}).text
+            except:
+                print(f"Error at: {url}")
+            listing_dict = {
+                'url': url,
+                'title': title,
+                'price': price,
+                'location': location,
+                'source': source,
+                'scrape_date':scrape_date,
+                'search_term':search_term
+            }
+            listings.append(listing_dict)
+
     #end = time.time()
     #print(end-start)
     browser.quit()
 
-    return listings
+    return listings[:result_limit-1]
 
 # scraped = scrape('backcountry skis', 101)
 # print(len(scraped))
